1b96f78756e6676a78f007c2805e54c3ce132c9a
jeedddiiii <pendigon.once@gmail.com>
2024-03-09 22:32:49 +0700

    Add transaction query

--- main.go
+++ main.go
@@ -2,18 +2,54 @@ package main
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Transaction struct {
+	TransactionID  int       `db:"transaction_id"`
+	Name           string    `db:"name"`
+	DateTime       time.Time `db:"date_time"`
+	SourceID       int       `db:"source_id"`
+	Emotion        string    `db:"emotion"`
+	FaceImg        string    `db:"face_img"`
+	EnvironmentImg string    `db:"environment_img"`
+}
+
 func main() {
 	// Create a new Gin router
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:5173"}
+	router.Use(cors.New(config))
+
 	connectDb()
 	// Define a route
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/transaction", func(c *gin.Context) {
+		rows, err := db.Query("SELECT * FROM transactions")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+
+		var transactions []Transaction // assuming Transaction is a struct that matches the table schema
+		for rows.Next() {
+			var t Transaction
+			err := rows.Scan(&t.TransactionID, &t.Name, &t.DateTime, &t.Emotion, &t.SourceID, &t.FaceImg, &t.EnvironmentImg)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			transactions = append(transactions, t)
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, this is your Gin server!",
+			"transactions": transactions,
 		})
 	})
 
